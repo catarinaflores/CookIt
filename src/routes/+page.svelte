0b55b6e48d97a30f6b99041bc29a918d926f8f87
catarinaflores <catarina10flores@gmail.com>
2025-05-12 15:32:36 +0100

    this a blockout yo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    import CookingStation from '$lib/cookingStation.svelte';
+</script>
+
+<main>
+    <header>
+        <h1>CookIt</h1>
+    </header>
+
+    <CookingStation />
+</main>
+
+<style>
+    header{
+        justify-content: center;
+        display: flex;
+        align-items: center;
+        background-color: black;
+    }
+
+    header h1{
+        color: white;
+    }
+    
+    main{
+        background-color: aqua;
+    }
+</style>
